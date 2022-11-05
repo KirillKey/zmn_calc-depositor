d8aed838e92390cb870bd9b4ea48d1e2d6b57419
Kirill Kuklin <kirill.km.work@gmail.com>
2022-11-05 19:15:11 +0300

    add base functional. percent - none

--- src/scripts/deposit-calc.js
+++ src/scripts/deposit-calc.js
@@ -1,39 +1,63 @@
 const depositInput = $('.deposit-calc__input');
-const calcDepositBtn = $('.calc-deposit-btn');  
-
-
-
-
-calcDepositBtn.on('click', function () {
-
-const initialSum = $('.calc-input--sum').val();
-const deadlines = $('.calc-input--deadlines').val();
-const bet = $('.calc-input--bet').val();
-const period = $('.calc-input--period').val();
-const addsDeposit = $('.calc-input--adds-deposit').val();
-
+const calcDepositBtn = $('.calc-deposit-btn');
 const finalSum = $('.deposit__result__out--final-sum');
 const allPercents = $('.deposit__result__out--all-percents');
 const sumAllDeposits = $('.deposit__result__out--sum-all-deposits');
+const calcInputCapitalization = document.querySelectorAll('.calc-input__capitalizations .capitalization');
+const calcInputDepositPeriod = document.querySelectorAll('.calc-input__deposit_period .deposit_period__val');
 
-const finalSumVal = Math.round(initialSum * (1 + bet));
-const test = bet / period;
-const test2 = bet / 2;
-const allPercentsVal = 1;
-const sumAllDepositsVal = 1;
+function clearFields(e) {
+    finalSum.text("")
+    allPercents.text("")
+    sumAllDeposits.text("")
+};
+
+function selectCapit (e) {  
+    for (let i = 0; i < calcInputCapitalization.length; i++) {
+        if (calcInputCapitalization[i].selected) {
+            if (calcInputCapitalization[i].dataset.capitalization) {
+                if (calcInputCapitalization[i].value == 0) {
+                    return 1;
+                } else return calcInputCapitalization[i].value;
+            }
+        }
+    }
+}
+function selectDeposit (e) {  
+    for (let i = 0; i < calcInputDepositPeriod.length; i++) {
+        if (calcInputDepositPeriod[i].selected) {
+            if (calcInputDepositPeriod[i].dataset.depositperiod) {
+                return calcInputDepositPeriod[i].value;
+            }
+        }
+    }
+}
+
+function initSumTest(sum) {
+    if (sum.length > 5) {
+        // `.0`
+   console.log(123);
+    }
+}
+
+calcDepositBtn.on('click', function (e) {
+    clearFields();
     
-    console.log(finalSumVal);
-    console.log(test);
-    console.log(test2);
-    // console.log(initialSum);
-    // console.log(bet);
+    const initialSum = parseFloat($('.calc-input__sum').val());
+    const deadlines = parseFloat($('.calc-input__deadlines').val());
+    const bet = parseFloat($('.calc-input__bet').val());
+    const capitalization = selectCapit();
+    const addDeposit = parseFloat($('.calc-input__adds-deposit').val());
+    const addDepositOption = selectDeposit();
+    // initSumTest(initialSum);
 
-    // finalSum.append(finalSumVal)
+
+    const finalSumVal = Math.floor(Math.pow(initialSum * (1 + (bet * 0.01) / capitalization), deadlines * capitalization));
+    const allPercentsVal = 1;
+    const sumAllDepositsVal = 1;
+    
+    // console.log( bet * 0.01 / 12);
+    finalSum.append(finalSumVal)
 });
 
-depositInput.on('input', function (e) {
-    if (e) {
-        
-    }
-    // alert('Вот так вот')
-})
+// Нужно подставить корректные значения процентов и т.п. (скоро допишу)
